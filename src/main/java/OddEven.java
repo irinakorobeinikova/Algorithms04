5d27b6b090b6899bc6a0a31a87998c344d2536ae
irinakorobeinikova <irinakor81@mail.ru>
2022-06-02 01:14:10 +0200

    add HW9Task1 OddEvenTest

--- src/main/java/OddEven.java
+++ src/main/java/OddEven.java
@@ -1,2 +1,33 @@
 public class OddEven {
+
+    /** Task 1
+     * Create an OddEven algorithm that takes an integer as input,
+     * returns "Odd" if the number is odd,
+     * and "Even" if the number is even.
+     * In all other cases, the result will be “Undefined”.
+     *
+     * Test Data:
+     * -345 →  “Odd”
+     * 0 →  “Even”
+     * 222222 →  “Even”
+     * 2147483647 + 1 →  “Undefined”
+     */
+
+    public String oddEvenAlgorithm(long number) {
+        String odd = "Odd";
+        String even = "Even";
+        String undefined = "Undefined";
+
+        if (number <= 2147483647L && number >= -2147483648L) {
+            if (number % 2 == 0) {
+
+                return even;
+            } else {
+
+                return odd;
+            }
+        }
+
+        return undefined;
+    }
 }
--- src/test/java/OddEvenTest.java
+++ src/test/java/OddEvenTest.java
@@ -1,2 +1,107 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
 public class OddEvenTest {
+
+    /** Create an OddEven algorithm that takes an integer as input,
+     * returns "Odd" if the number is odd,
+     * and "Even" if the number is even.
+     * In all other cases, the result will be “Undefined”.
+     *
+     * Test Data:
+     * -345 →  “Odd”
+     * 0 →  “Even”
+     * 222222 →  “Even”
+     * 2147483647 + 1 →  “Undefined”
+     */
+
+    @Order(1)
+    @Test
+    public void testOddEvenHappyPathPositiveOddNumber() {
+        long number = 1;
+        String expectedResult = "Odd";
+
+        OddEven as = new OddEven();
+        String actualResult = as.oddEvenAlgorithm(number);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Order(2)
+    @Test
+    public void testOddEvenHappyPathPositiveEvenNumber() {
+        long number = 2;
+        String expectedResult = "Even";
+
+        OddEven as = new OddEven();
+        String actualResult = as.oddEvenAlgorithm(number);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Order(3)
+    @Test
+    public void testOddEvenHappyPathNegativeOddNumber() {
+        long number = -1;
+        String expectedResult = "Odd";
+
+        OddEven as = new OddEven();
+        String actualResult = as.oddEvenAlgorithm(number);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Order(4)
+    @Test
+    public void testOddEvenHappyPathNegativeEvenNumber() {
+        long number = -2;
+        String expectedResult = "Even";
+
+        OddEven as = new OddEven();
+        String actualResult = as.oddEvenAlgorithm(number);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Order(5)
+    @Test
+    public void testOddEvenHappyPathZeroNumber() {
+        long number = 0;
+        String expectedResult = "Even";
+
+        OddEven as = new OddEven();
+        String actualResult = as.oddEvenAlgorithm(number);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testOddEvenNegativeUndefined() {
+        long number = -2147483649L;
+        String expectedResult = "Undefined";
+
+        OddEven as = new OddEven();
+        String actualResult = as.oddEvenAlgorithm(number);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testOddEvenPositiveUndefined() {
+        long number = 2147483648L;
+        String expectedResult = "Undefined";
+
+        OddEven as = new OddEven();
+        String actualResult = as.oddEvenAlgorithm(number);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+
+
+
+
+
+
 }
